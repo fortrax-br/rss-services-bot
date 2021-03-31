fd9b5699a94b030f0afe1b3cb2bc17e9782d2ea8
dheisom-gomes <dheisom9@gmail.com>
2021-03-31 20:38:51 -0300

    Correção de erros

--- bot/app.py
+++ bot/app.py
@@ -1,22 +1,20 @@
-from json import load
-
-import database
-from os import environ
-from callbacks.handler import handler
 from pyrogram import Client
+from os import environ
+from database import crub
 from pyrogram.handlers import CallbackQueryHandler
+from callbacks.handler import handler as callbacksHandler
 
-App: Client = Client(
+
+App = Client(
     "RSS",
     api_id=int(environ.get("TELEGRAM_API_ID")),
     api_hash=environ.get("TELEGRAM_API_HASH"),
     bot_token=environ.get("BOT_TOKEN")
 )
-
-App.database = database.crub(
-    environ.get(
+App.database = crub(
+    url=environ.get(
         "DATABASE_URL",
         "sqlite:///rssBot.db"
     )
 )
-App.add_handler(CallbackQueryHandler(handler))
+App.add_handler(CallbackQueryHandler(callbacksHandler))
